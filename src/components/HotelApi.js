e278922ea27be37cc3a090c311c23493b95254ae
Rainis Randmaa <raintz1000@gmail.com>
2022-11-27 17:00:51 +0200

    Replace sample data with API requests

--- src/components/HotelApi.js
+++ src/components/HotelApi.js
@@ -1,3 +1,5 @@
+const prefix = ''
+
 /***
  * @typedef {Object} Room
  * @property {String} name Type of room
@@ -10,59 +12,7 @@
  * @returns {Room[]}
  */
 async function getRooms() {
-  return await [
-    {
-      name: "Ühekohaline",
-      available: 4,
-      price: 40,
-      pictures: [
-        "https://images.trvl-media.com/lodging/75000000/74250000/74242600/74242531/9b2b2d66.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/5a476c74.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/9c4ce050.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Kahekohaline",
-      available: 5,
-      price: 75,
-      pictures: [
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/1436d39c.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/bf3c0cbc.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/79035584.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Kahe voodiga",
-      available: 3,
-      price: 80,
-      pictures: [
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/83c80c39.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/917201ab.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/c43c9335.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Kolmekohaline",
-      available: 3,
-      price: 100,
-      pictures: [
-        "https://images.trvl-media.com/lodging/75000000/74250000/74242600/74242531/1331f409.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/42361b97.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/11995beb.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Sviit",
-      available: 1,
-      price: 150,
-      pictures: [
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/9f0abbf8.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/bcb18a26.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/5dfe5424.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/f5bad581.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-  ];
+    return (await fetch(prefix + '/api/rooms')).json();
 }
 
 /***
@@ -71,63 +21,9 @@ async function getRooms() {
  * @returns {Room[]}
  */
 async function getRoomsSortedBy(column, isAscending) {
-  return await [
-    {
-      name: "Ühekohaline",
-      available: 4,
-      price: 40,
-      pictures: [
-        "https://images.trvl-media.com/lodging/75000000/74250000/74242600/74242531/9b2b2d66.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/5a476c74.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/9c4ce050.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Kahekohaline",
-      available: 5,
-      price: 75,
-      pictures: [
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/1436d39c.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/bf3c0cbc.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/79035584.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Kahe voodiga",
-      available: 3,
-      price: 80,
-      pictures: [
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/83c80c39.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/917201ab.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/c43c9335.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Kolmekohaline",
-      available: 3,
-      price: 100,
-      pictures: [
-        "https://images.trvl-media.com/lodging/75000000/74250000/74242600/74242531/1331f409.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/42361b97.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/11995beb.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Sviit",
-      available: 1,
-      price: 150,
-      pictures: [
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/9f0abbf8.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/bcb18a26.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/5dfe5424.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/f5bad581.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-  ].sort((roomA, roomB) => {
-    if (roomA[column] < roomB[column]) return isAscending ? -1 : 1;
-    if (roomA[column] > roomB[column]) return isAscending ? 1 : -1;
-    return 0;
-  });
+  return (await fetch(prefix + '/api/rooms-sorted?' + new URLSearchParams({
+      column, isAscending
+    }))).json();
 }
 
 /***
@@ -136,59 +32,9 @@ async function getRoomsSortedBy(column, isAscending) {
  * @returns {Room[]}
  */
 async function getRoomsByAvailability(startDate, endDate) {
-  return await [
-    {
-      name: "Ühekohaline",
-      available: Math.floor(Math.random() * 7),
-      price: 40,
-      pictures: [
-        "https://images.trvl-media.com/lodging/75000000/74250000/74242600/74242531/9b2b2d66.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/5a476c74.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/9c4ce050.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Kahekohaline",
-      available: Math.floor(Math.random() * 7),
-      price: 75,
-      pictures: [
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/1436d39c.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/bf3c0cbc.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/79035584.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Kahe voodiga",
-      available: Math.floor(Math.random() * 5),
-      price: 80,
-      pictures: [
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/83c80c39.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/917201ab.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/c43c9335.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Kolmekohaline",
-      available: Math.floor(Math.random() * 5),
-      price: 100,
-      pictures: [
-        "https://images.trvl-media.com/lodging/75000000/74250000/74242600/74242531/1331f409.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/42361b97.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/11995beb.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-    {
-      name: "Sviit",
-      available: Math.floor(Math.random() * 2),
-      price: 150,
-      pictures: [
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/9f0abbf8.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/bcb18a26.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/5dfe5424.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-        "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/f5bad581.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
-      ],
-    },
-  ];
+  return (await fetch(prefix + '/api/rooms-available?' + new URLSearchParams({
+      startDate, endDate
+    }))).json();
 }
 
 export { getRooms };
